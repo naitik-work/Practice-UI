2cc00cf7c925a7bbba0239c2ffc869a0a4105eb9
naitik-cmd <naitikchs16@gmail.com>
2025-12-11 15:28:03 +0530

    then and catch

--- Js/Promises/script.js
+++ Js/Promises/script.js
@@ -3,15 +3,27 @@
 //hojayega->resolved
 //nahi hoga-> reject
 
-const prm= new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-        resolve();
-    },3000);
-});
+// const prm= new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         resolve();
+//     },3000);
+// });
 
-prm.then(function(){
-    console.log('heyheyhey')
-});
-prm.catch(function(){
-    console.log('hiehiheih')
-});
+// prm.then(function(){
+//     console.log('heyheyhey')
+// });
+// prm.catch(function(){
+//     console.log('hiehiheih')
+// });
+
+fetch(`https://randomuser.me/api/`)
+.then(function(nonReadableData){
+     return nonReadableData.json();
+})
+.then(function(realData){
+    console.log(realData.results[0].name.first);
+
+})
+.catch(function(err){
+    console.log(err);
+})
