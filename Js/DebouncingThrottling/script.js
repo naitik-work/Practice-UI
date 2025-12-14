b30dd3b282c0872579a6d650fb57a7e1b6c6e1e9
naitik-cmd <naitikchs16@gmail.com>
2025-12-15 01:29:42 +0530

    throtttling

--- Js/DebouncingThrottling/script.js
+++ Js/DebouncingThrottling/script.js
@@ -2,14 +2,33 @@
 //jab koi insan type krra ho toh kuch fixed limited time ke baad hee suggestions show kre, i.e debouncing.
 
 
-function debounce(fn,delay){
-    let timer;
+// function debounce(fn,delay){
+//     let timer;
+//     return function(){
+//         clearTimeout(timer);
+//         timer= setTimeout(fn,delay);
+//     };
+// };
+
+// document.querySelector('#search').addEventListener('input', debounce(function(){
+//     console.log('chala');
+// },2000));
+
+
+//throttling
+//for any event, kuch fixed interval me ek baar function update hoga. for eg. mouse move wala scenario yaad kro, har 2 sec me e.ClientX aur ClientY ki position console pe print krwa rhe the...
+
+function throttle(fn, delay){
+    let last=0;
     return function(){
-        clearTimeout(timer);
-        timer= setTimeout(fn,delay);
+        const now= Date.now();
+        if(now-last>=delay){
+            last=now;
+            fn();
+        }
     };
 };
 
-document.querySelector('#search').addEventListener('input', debounce(function(){
-    console.log('chala');
-},2000));
+window.addEventListener('mousemove', throttle(function(){
+    console.log('Hey NaitiK!'),20})
+);
