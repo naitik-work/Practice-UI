49d7aeb189fc43641ff4497c7a096c69184bf8c5
naitik-cmd <naitikchs16@gmail.com>
2025-12-14 16:01:35 +0530

    mail merge api

--- Js/WhetherAPI/script.js
+++ Js/WhetherAPI/script.js
@@ -20,26 +20,65 @@
 //using async await
 
 
-async function whethercheck(city){
-    try{
-        let apikey=`1ddbc6e263034e0a81ee6bf6f4b4408d`;
-        let raw =await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&exclude={part}&appid=${apikey}`)
-        if(!raw.ok){
-            throw new Error('The city name is incorrect. SOMETHING WENT WRONG!');
-        }
-        let realData= await raw.json();
-        console.log(realData);
-        if((realData.main.temp-273)>32){
-            console.warn(`TOO hot to handle....${Math.floor(realData.main.temp-273)}°C`)
-        }
-        if((realData.main.temp-273)<0){
-            console.warn(`TOO Cold to handle....${Math.floor(realData.main.temp-273)}°C`)
-        }
-        console.log(`The temperature of the city ${city} is: ${Math.floor(realData.main.temp-273)}°C `)
-    }
-    catch(err){
-        console.log(err.message);
-    }
+// async function whethercheck(city){
+//     try{
+//         let apikey=`1ddbc6e263034e0a81ee6bf6f4b4408d`;
+//         let raw =await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&exclude={part}&appid=${apikey}`)
+//         if(!raw.ok){
+//             throw new Error('The city name is incorrect. SOMETHING WENT WRONG!');
+//         }
+//         let realData= await raw.json();
+//         console.log(realData);
+//         if((realData.main.temp-273)>32){
+//             console.warn(`TOO hot to handle....${Math.floor(realData.main.temp-273)}°C`)
+//         }
+//         if((realData.main.temp-273)<0){
+//             console.warn(`TOO Cold to handle....${Math.floor(realData.main.temp-273)}°C`)
+//         }
+//         console.log(`The temperature of the city ${city} is: ${Math.floor(realData.main.temp-273)}°C `)
+//     }
+//     catch(err){
+//         console.log(err.message);
+//     }
 
+// }
+// whethercheck('Allahabad');
+
+const users=['naitik12@gmail.com',
+            'ajfh@gmail.com',
+            'fahouoh@gmail.com',
+            'aadadw@gmail.com'];
+
+function sendEmail(email){
+    return new Promise((resolve,reject)=>{
+        let time= Math.floor(Math.random()*5);
+        setTimeout(()=>{
+            let probability= Math.floor(Math.random()*10);
+            if(probability<=5){
+                resolve('Email sent successfully!');
+            }
+            else{
+                reject('Email not sent!')
+            }
+        },time*1000)
+    })
 }
-whethercheck('Allahabad');
+
+
+async function sendEmails(users){
+    let allresponses= users.map(function(email){
+        return sendEmail(email)
+        .then(function(abs){
+            console.log(abs);
+        })
+        .catch(function(err){
+            console.log(err);
+        });
+    })
+    let ans= await Promise.all(allresponses);
+    ans.forEach(function(result){
+        console.log(result);
+    })
+}
+
+sendEmails(users);
